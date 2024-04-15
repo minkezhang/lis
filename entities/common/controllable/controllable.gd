a166abe4567850ab25ff9681c0dd077deef76a62
Minke Zhang <minke.zhang@gmail.com>
2024-04-15 02:00:32 -0700

    Make controller input smoother

--- entities/common/controllable/controllable.gd
+++ entities/common/controllable/controllable.gd
@@ -5,7 +5,7 @@ signal input_south
 signal input_east
 signal input_west
 
-const _POLL_RATE_HZ: float = 5
+const _POLL_RATE_HZ: float = 8
 const _POLL_RATE_DELTA: float = 1.0 / _POLL_RATE_HZ
 
 var _delta_accum: float = 0
@@ -16,13 +16,17 @@ func _process(delta):
 		_delta_accum += delta
 		return
 	
-	_delta_accum = 0
+	_delta_accum = max(_delta_accum, 1)
 	
 	if Input.is_action_pressed('ui_right'):
 		input_east.emit()
+		_delta_accum = 0
 	if Input.is_action_pressed('ui_left'):
 		input_west.emit()
+		_delta_accum = 0
 	if Input.is_action_pressed('ui_up'):
 		input_north.emit()
+		_delta_accum = 0
 	if Input.is_action_pressed('ui_down'):
 		input_south.emit()
+		_delta_accum = 0
--- entities/max/max.gd
+++ entities/max/max.gd
@@ -8,3 +8,6 @@ func _ready():
 	$Controllable.input_south.connect(func(): path_queue.enqueue(_libgeo.Orientation.S))
 	$Controllable.input_east.connect(func(): path_queue.enqueue(_libgeo.Orientation.E))
 	$Controllable.input_west.connect(func(): path_queue.enqueue(_libgeo.Orientation.W))
+
+func _process(_delta):
+	super(_delta)
