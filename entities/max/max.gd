a4147034c42aad4fcd2fb86b7f36ea3627b34472
Minke Zhang <minke.zhang@gmail.com>
2024-04-18 01:44:19 -0700

    Make Max a subclass of Character instead to simplify tree

--- entities/max/max.gd
+++ entities/max/max.gd
@@ -1,10 +1,9 @@
-extends Node2D
-
-const _libgeo = preload('res://lib/geo.gd')
+extends Character
 
 
 func _ready():
-	$Controllable.input_north.connect(func(): $Character.path_queue.enqueue(_libgeo.Orientation.N))
-	$Controllable.input_south.connect(func(): $Character.path_queue.enqueue(_libgeo.Orientation.S))
-	$Controllable.input_east.connect(func(): $Character.path_queue.enqueue(_libgeo.Orientation.E))
-	$Controllable.input_west.connect(func(): $Character.path_queue.enqueue(_libgeo.Orientation.W))
+	super()
+	$Controllable.input_north.connect(func(): path_queue.enqueue(_libgeo.Orientation.N))
+	$Controllable.input_south.connect(func(): path_queue.enqueue(_libgeo.Orientation.S))
+	$Controllable.input_east.connect(func(): path_queue.enqueue(_libgeo.Orientation.E))
+	$Controllable.input_west.connect(func(): path_queue.enqueue(_libgeo.Orientation.W))
